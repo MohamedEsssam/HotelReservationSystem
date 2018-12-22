410864f99dd79e9ccf06df89770fb778105dce3f
omar_swidan <oswidan97@gmail.com>
2018-12-22 23:47:39 +0200

    fixed conflict for revert

--- controller/registrationController.js
+++ controller/registrationController.js
@@ -4,9 +4,10 @@ module.exports = (req, res) => {
     console.log(req.session.registrationErrors)
     const { username, password,accountType } = req.body;
   
+   res.sendFile(path.resolve(__dirname,"../views/RegistrationForm.html"))
     //res.render('register')
-    res.render('RegistrationForm',{
-        //flashing the errors to the next request only
-        errors:req.flash('registrationErrors')
-    })
+    // res.render('RegistrationForm',{
+    //     //flashing the errors to the next request only
+    //     errors:req.flash('registrationErrors')
+    // })
 };
