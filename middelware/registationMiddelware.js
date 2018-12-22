b0dd6f9f073d8f16157f39da5392838981063562
omar_swidan <oswidan97@gmail.com>
2018-12-22 23:29:12 +0200

    Revert "fixed conflicts"
    This reverts commit 77962dd66441cd43d725120f59f64f69276e6138.

--- middelware/registationMiddelware.js
+++ middelware/registationMiddelware.js
@@ -1,7 +1,12 @@
 const db=require('../models/index')
 
 module.exports=(req,res,next)=>{
+<<<<<<< HEAD
     db[req.body.selectedType].findOne({ where: { username : req.body.username } }).then(user => {
+=======
+    const {username} = req.body
+    db[req.body.selectedType].findOne({ where: { username : username } }).then(user => {
+>>>>>>> 0eb0a5147bc1fb373bb93b2b866896f0fe7d2836
         if (user) {
                 res.redirect('/register')
         }
