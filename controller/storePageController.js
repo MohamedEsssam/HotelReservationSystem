fed4d6cfff0a068a6771c0e6b72b6157388dcf16
Mostafa Yousry <mostafayousry8@gmail.com>
2018-12-23 16:06:08 +0200

    add heroku package

--- controller/storePageController.js
+++ controller/storePageController.js
@@ -5,15 +5,9 @@ module.exports = (req, res) => {
     console.log('************************')
    const db = require('../models/index')
    
-<<<<<<< HEAD:controller/storeUserController.js
-   const { username,name,ssn,email,telephone,password,credit} = req.body;
-  
-   console.log(username+ ' ' + name+ ' ' + ssn+ ' ' + email+ ' ' + telephone+ ' ' + password + ' ' + credit + ' '+ req.body.selectedType)
-=======
    const { username,name,ssn,email,phone,password,credit} = req.body;
    console.log("GSDGSDG");
    console.log(username+ ' ' + name+ ' ' + ssn+ ' ' + email+ ' ' + phone+ ' ' + password + ' ' + credit)
->>>>>>> 27c336ed27c9dde945780a3f5b42abc611b42dbe:controller/storePageController.js
    
    const UserModel = db[req.body.selectedType]
 
