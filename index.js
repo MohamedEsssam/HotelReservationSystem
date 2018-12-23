4d8be838cd3d398911460c3941cb54874d427df3
Mostafa Yousry <mostafayousry8@gmail.com>
2018-12-23 17:13:20 +0200

    try to fix heroku connections problem

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.get('/hotelManager/:username', hotelManagerPage)
 //login page
 app.post('/login',loginController);
 
-app.get('/', homeController)
+app.get('*', homeController)
 
 app.get('/favicon.ico', homeController)
 
